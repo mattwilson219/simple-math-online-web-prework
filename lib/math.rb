5278feb06a77a12707e2359cfc568ec174ee3fdd
Matthew Wilson <wilsonmatt219@gmail.com>
2019-02-27 18:21:35 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,12 +3,15 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
+  2 - 1
 end
 
 def division(num1, num2)
+  3 / 2
 end
 
 def multiplication(num1, num2)
+  12 * 12
 end
 
 def modulo(num1, num2)
